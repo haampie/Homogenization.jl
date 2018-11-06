4ad836f21ac070bd698a91f61995d9d44661f346
autodocs <autodocs>
2018-11-06 12:32:02 +0000

    build based on f87dbfa

---
+++ dev/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#Homogenization.jl-1",
+    "page": "Home",
+    "title": "Homogenization.jl",
+    "category": "section",
+    "text": "Hello world."
+},
+
+]}
